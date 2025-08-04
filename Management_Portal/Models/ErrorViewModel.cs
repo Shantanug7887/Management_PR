4e5f00f851ce64dcd0bc0b91741703d8846b127c
Shantanu Gawad <Shantanug7887@gmail.com>
2025-08-04 23:22:26 +0530

    Add project files.

---
+++ Management_Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Management_Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
